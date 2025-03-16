9a40ff77b36f26730937c22ce96fa41fa6cdc154
Michał Napora <napora.michael@gmail.com>
2025-03-16 07:25:30 +0100

    Updated

--- android/app/src/main/java/com/crawler/mystic/mystic_crawler/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.crawler.mystic.mystic_crawler;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/crawler/mystic/mystic_crawler/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.crawler.mystic.mystic_crawler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
